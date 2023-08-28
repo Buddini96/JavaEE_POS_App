0960d72355d0a6ac5b0b1f7c6546f18b673a8781
buddini96 <buddinikl1996@gmail.com>
2023-08-28 16:17:27 +0530

    Updated

--- Back_End/src/lk/ijse/pos/servlet/PurchaseOrderServletAPI.java
+++ Back_End/src/lk/ijse/pos/servlet/PurchaseOrderServletAPI.java
@@ -36,7 +36,7 @@ public class PurchaseOrderServletAPI extends HttpServlet {
                 arrayBuilder.add(code.build());
             }
 
-            PreparedStatement cusPstm = connection.prepareStatement("SELECT id FROM customer");
+            PreparedStatement cusPstm = connection.prepareStatement("SELECT cusID FROM customer");
             ResultSet cusDetails = cusPstm.executeQuery();
 
             CustomerDTO customerDTO = new CustomerDTO();
@@ -75,7 +75,7 @@ public class PurchaseOrderServletAPI extends HttpServlet {
             CustomerDTO customerDTO = new CustomerDTO();
             customerDTO.setId(id);
 
-            PreparedStatement pstm = connection.prepareStatement("SELECT * FROM customer WHERE id=?");
+            PreparedStatement pstm = connection.prepareStatement("SELECT * FROM customer WHERE cusID=?");
             pstm.setObject(1,customerDTO.getId());
 
             ResultSet resultSet = pstm.executeQuery();
@@ -116,7 +116,7 @@ public class PurchaseOrderServletAPI extends HttpServlet {
             ItemDTO itemDTO = new ItemDTO();
             itemDTO.setCode(code);
 
-            PreparedStatement pstm = connection.prepareStatement("SELECT * FROM item WHERE code=?");
+            PreparedStatement pstm = connection.prepareStatement("SELECT * FROM item WHERE itemCode=?");
             pstm.setObject(1,itemDTO.getCode());
 
             ResultSet resultSet = pstm.executeQuery();
